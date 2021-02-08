cb0a37f538c5eb8d816ffb88229fbb4960d4c22b
6kamens <slil.puangpoom@gmail.com>
2021-02-09 01:39:33 +0700

    UI : landing page content

--- src/features/landing/header/index.jsx
+++ src/features/landing/header/index.jsx
@@ -1,33 +1,35 @@
 import React from "react";
+import landingBg from "../../../common/img/landing-main-top.jpg";
+
 
 function Header() {
   return (
     <div className="container mx-auto">
       <div className="flex flex-col-reverse lg:flex-row justify-between items-center">
-        <div className="flex flex-1 justify-center text-center sm:text-left mt-10">
+        <div className="flex flex-1 justify-center text-center lg:text-left mt-10">
           <div>
-            <h1 className="lg:text-3xl text-2xl">
-              E-commerce รูปเเบบใหม่ได้เเล้ววันนี้
+            <h1 className="lg:text-6xl text-4xl font-extrabold">E-commerce</h1>
+            <h1 className="lg:text-6xl text-4xl font-extrabold lg:pl-16">
+              <i className="fas fa-plus-circle px-3 text-pink-500"></i>
             </h1>
-            <h1 className="lg:text-3xl text-2xl">
-              ค้นหาร้านค้าที่ใช่สำหรับคุณ
+            <h1 className="lg:text-6xl text-4xl font-extrabold">Marketplace</h1>
+            <h1 className="lg:text-2xl text-xl">
+              ค้นหา Market เเล้วไปช๊อปกันเลย !
             </h1>
             <div className="mb-4">
-              <input className="border py-2 px-3 text-grey-darkest"></input>
+              <input
+                maxLength="25"
+                className="text-lg border py-2 px-3 text-grey-darkest border-gray-500"
+              ></input>
             </div>
-            <button className="text-white text-lg bg-pink-500 hover:bg-blue-600 shadow-md focus:outline-none p-2 rounded">
-              <i className="fas fa-users mr-2"></i>
-              ค้นหาร้านค้า
+            <button className="text-white text-lg bg-pink-500 hover:bg-pink-400 shadow-md focus:outline-none p-2 rounded">
+              <i className="fas fa-shopping-bag mr-2"></i>
+              ค้นหา Market
             </button>
           </div>
         </div>
         <div className="flex flex-1 justify-center">
-          <img
-            src={
-              "https://image.freepik.com/free-vector/launching-concept-illustration_114360-1731.jpg"
-            }
-            className="w-2/3"
-          ></img>
+          <img src={landingBg} className="w-2/3"></img>
         </div>
       </div>
     </div>
--- src/features/landing/index.jsx
+++ src/features/landing/index.jsx
@@ -2,86 +2,73 @@ import React from "react";
 import Header from "./header";
 import Event from "./event";
 import Footer from "./footer";
-
+import questionIcon from "../../common/icon/question.svg";
+import moneyIcon from "../../common/icon/money.svg";
+import trustIcon from "../../common/icon/trust.svg";
+import productIcon from "../../common/icon/product.svg";
 
 function Landing(props) {
+  const subSectionContent = [
+    {
+      title: "การจัดสินค้า",
+      content:"สินค้าเเยกตาม marketplace ไม่ใช่ตามประเภทของสินค้า เสมือนคุณได้เข้าไปดูสินค้าใน marketplace จริงๆ",
+      icon:productIcon
+    },
+    {
+      title: "ความน่าเชื่อถือ",
+      content:"ซื้อของเหมือนอยู่ใน marketplace เเต่ได้ประกันสินค้าเสมือนคุณซื้อผ่านร้าน e-commerce ที่เป็นทางการ",
+      icon:trustIcon
+    },
+    {
+      title: "คนซื้อ = คนขาย",
+      content:"คุณไม่เพียงสามารถซื้อสินค้า เเต่ยังสามารถเป็นผู้ขายสินค้าเองได้ด้วย เป็นช่องทางสร้างรายได้ให้กับคุณ",
+      icon:moneyIcon
+    },
+  ];
+
   return (
     <div className="m-0 p-0">
       <Header></Header>
       <div className="bg-gray-50 mt-10">
         <div className="flex flex-col lg:flex-row container mx-auto p-5 lg:p-10">
           <div className="flex-1 text-center lg:border-r-2  border-gray-400 p-5 pb-10">
-            <img
-              src={
-                "https://www.flaticon.com/svg/vstatic/svg/1170/1170678.svg?token=exp=1611812017~hmac=f49a40415eb14008659028758aae7011"
-              }
-              className="w-1/6 mx-auto mb-4"
-            ></img>
+            <img src={questionIcon} className="w-1/6 mx-auto mb-4"></img>
             <h1 className="lg:text-3xl text-2xl text-indigo-400">
               เราคือใคร ?
             </h1>
             <p className="text-lg m-0">
-              เราเป็นเเหล่งรวม marketplace
-              มาให้ท่านได้เลือกสรรอย่างที่ท่านต้องการ
-            </p>
-            <p className="text-lg m-0">
-              Passion for drive สำหรับชุมชนที่ต้องการเช้าร่วยกับเรา
-              สมัครมาได้เลย
+              เราสร้างประสบการณ์ใหม่ในการซื้อสินค้า โดยนำ marketplace
+              มารวมกับบริการ e-commerce ด้วยเเนวคิดใหม่เพื่อให้ท่านสามารถเข้าถึง
+              marketplace ได้ง่ายเเละสะดวกกว่าที่เคยเป็นมา
             </p>
           </div>
           <div className="flex justify-center flex-1  border-t-2 lg:border-none border-gray-400  p-5 pt-10 pb-10">
             <div className="lg:w-2/3">
-              <div className="flex flex-row justify-center items-center space-x-8 mb-3">
-                <img
-                  src={
-                    "https://www.flaticon.com/svg/vstatic/svg/1170/1170678.svg?token=exp=1611812017~hmac=f49a40415eb14008659028758aae7011"
-                  }
-                  className="w-16"
-                ></img>
-                <div>
-                  <h1 className="text-xl text-indigo-400 mb-0">เราคือใคร ?</h1>
-                  <span>
-                    มาเข้าร่วมกับเราได้เเล้ววันนี้ มาเลย
-                    มาเข้าร่วมกับเราได้เเล้ววันนี้
-                  </span>
-                </div>
-              </div>
-              <div className="flex flex-row justify-center items-center space-x-8 mb-3">
-                <img
-                  src={
-                    "https://www.flaticon.com/svg/vstatic/svg/1170/1170678.svg?token=exp=1611812017~hmac=f49a40415eb14008659028758aae7011"
-                  }
-                  className="w-16"
-                ></img>
-                <div>
-                  <h1 className="text-xl text-indigo-400 mb-0">เราคือใคร ?</h1>
-                  <span>
-                    มาเข้าร่วมกับเราได้เเล้ววันนี้ มาเลย
-                    มาเข้าร่วมกับเราได้เเล้ววันนี้
-                  </span>
-                </div>
-              </div>
-              <div className="flex flex-row justify-center items-center space-x-8">
-                <img
-                  src={
-                    "https://www.flaticon.com/svg/vstatic/svg/1170/1170678.svg?token=exp=1611812017~hmac=f49a40415eb14008659028758aae7011"
-                  }
-                  className="w-16"
-                ></img>
-                <div>
-                  <h1 className="text-xl text-indigo-400 mb-0">เราคือใคร ?</h1>
-                  <span>
-                    มาเข้าร่วมกับเราได้เเล้ววันนี้ มาเลย
-                    มาเข้าร่วมกับเราได้เเล้ววันนี้
-                  </span>
-                </div>
-              </div>
+              {subSectionContent.map((content) => (
+                <SubSection
+                title={content.title}
+                content={content.content}
+                icon={content.icon}
+                ></SubSection>
+              ))}
             </div>
           </div>
         </div>
       </div>
       <Event></Event>
       <Footer></Footer>
+    </div>
+  );
+}
+
+function SubSection(props) {
+  return (
+    <div className="flex flex-row justify-center items-center space-x-8 mb-3">
+      <img src={props.icon} className="w-16"></img>
+      <div>
+        <h1 className="text-xl text-indigo-400 mb-0">{props.title}</h1>
+        <span>{props.content}</span>
+      </div>
     </div>
   );
 }
